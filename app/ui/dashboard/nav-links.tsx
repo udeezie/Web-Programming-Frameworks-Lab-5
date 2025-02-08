141caed04800bec97a1cd0b7e706979a13627003
Uriel <vortia123@gmail.com>
2025-02-08 00:14:09 -0500

    Lab 5

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,4 @@
-'use client'; // Required to use React hooks like usePathname
-
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -9,20 +8,26 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation
+
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname(); // Get the current path
-
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
-        const LinkIcon = link.icon; // Dynamically assign the icon component
+        const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
@@ -30,10 +35,9 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href, // Highlight active link
-              }
-            )}
-          >
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
